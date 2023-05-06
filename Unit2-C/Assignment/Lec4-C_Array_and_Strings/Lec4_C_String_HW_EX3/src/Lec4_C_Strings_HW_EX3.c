8e07683eacaa414d7268ca6dab6ce7b4541c5e5b
Thomas Medhat Mounir <thomasmounir99@gmail.com>
2023-05-06 20:17:21 +0300

    add Lec4_C_String_HW_EX

---
+++ Unit2-C/Assignment/Lec4-C_Array_and_Strings/Lec4_C_String_HW_EX3/src/Lec4_C_Strings_HW_EX3.c
@@ -0,0 +1,31 @@
+/*
+ ============================================================================
+ Name        : Lec4_C_Strings_HW_EX3.c
+ Author      : Thomas Medhat
+ Date        : May 6 , 2023
+ Description : Write a C program to reverse String without using library function
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	char st[100],revST[100],len = 0,i = 0;
+	printf("Enter a string : ");
+	fflush(stdin);
+	fflush(stdout);
+	gets(st);
+	while(st[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0 ; i < len ; i++)
+	{
+		revST[len-i-1] = st[i];
+	}
+	revST[i] = '\0';
+	printf("Reverse string is : %s\n",revST);
+	return 0 ;
+}
